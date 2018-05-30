312207c39226ec5eba4ce6dfb833f1deedfd60d6
Land Lu <v-luting@microsoft.com>
2018-05-30 17:11:23 +0800

    Add project files.

---
+++ ContainerViewDemo/ContainerViewController.cs
@@ -0,0 +1,44 @@
+﻿using Foundation;
+using System;
+using UIKit;
+
+namespace ContainerViewDemo
+{
+    public partial class ContainerViewController : UIViewController
+    {
+        public ContainerViewController (IntPtr handle) : base (handle)
+        {
+        }
+
+        UIViewController lastViewController;
+        private void addSubsFrom(string viewControllersIdentifier)
+        {
+            if (lastViewController != null)
+            {
+                lastViewController.View.RemoveFromSuperview();
+                lastViewController.RemoveFromParentViewController();
+            }
+
+            var viewController = Storyboard.InstantiateViewController(viewControllersIdentifier);
+            viewController.View.Frame = View.Bounds;
+
+            View.AddSubview(viewController.View);
+            AddChildViewController(viewController);
+
+            lastViewController = viewController;
+        }
+
+        public void segueIdentifireRecievedFromParent(int selectedRow)
+        {
+            if (selectedRow == 0)
+            {
+                addSubsFrom("TestAVC");
+
+            }
+            else if (selectedRow == 1)
+            {
+                addSubsFrom("TestBVC");
+            }
+        }
+    }
+}
---
+++ ContainerViewDemo/ViewController.cs
@@ -0,0 +1,67 @@
+﻿using Foundation;
+using System;
+using UIKit;
+
+namespace ContainerViewDemo
+{
+    public partial class ViewController : UIViewController
+    {
+
+        ContainerViewController containerViewController;
+
+		public ViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+            MyTableView.Source = new MyTableSource(containerViewController);
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+        public override void PrepareForSegue(UIStoryboardSegue segue, NSObject sender)
+        {
+            if (segue.Identifier == "Container")
+            {
+                containerViewController = segue.DestinationViewController as ContainerViewController;
+            }
+        }
+    }
+
+
+    public class MyTableSource : UITableViewSource
+    {
+ContainerViewController container;
+
+public MyTableSource(ContainerViewController viewController)
+{
+    container = viewController;
+}
+
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+            UITableViewCell cell = tableView.DequeueReusableCell("Cell");
+
+            cell.TextLabel.Text = "item" + indexPath.Row;
+
+            return cell;
+        }
+
+        public override nint RowsInSection(UITableView tableview, nint section)
+        {
+            return 2;
+        }
+
+        public override void RowSelected(UITableView tableView, NSIndexPath indexPath)
+        {
+            container.segueIdentifireRecievedFromParent(indexPath.Row);
+        }
+    }
+}
